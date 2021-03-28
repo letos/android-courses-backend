5dbcea4d152654e2e9c8b05e35e1aababf4d03bd
Letos <inralitrust@gmail.com>
2021-03-28 21:05:03 +0300

    don't duplicate favorite and rating

--- app/Http/Controllers/FavoriteController.php
+++ app/Http/Controllers/FavoriteController.php
@@ -5,16 +5,23 @@ namespace App\Http\Controllers;
 
 
 use App\Models\FavoriteItem;
+use App\Models\Item;
 use Illuminate\Support\Facades\Auth;
 
 class FavoriteController extends Controller
 {
     public function create($itemId)
     {
-        $favorite = new FavoriteItem;
-        $favorite->user_id = Auth::id();
-        $favorite->item_id = $itemId;
-        $favorite->save();
+        $userId = Auth::id();
+        $item = Item::find($itemId);
+
+        if (!$item->hasFavoriteUser($userId)) {
+            $favorite = new FavoriteItem;
+            $favorite->user_id = Auth::id();
+            $favorite->item_id = $itemId;
+            $favorite->save();
+        }
+
         return response()->noContent(200);
     }
 
